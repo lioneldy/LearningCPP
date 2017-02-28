6a3c90d44af710ee5ba0b7c49ba9c37d6475a0b8
lioneldy <407654339@qq.com>
2017-02-28 11:17:07 +0800

    新增17题代码以及机器学习笔记

--- LearningCPP/main.cpp
+++ LearningCPP/main.cpp
@@ -250,8 +250,63 @@ int threeSumClosest(vector<int>& nums, int target) {
     return ans;
 }
 
+//最大公约数
+int gcd(int a, int b) {
+    return (a%b==0) ? b : gcd(b,a%b);
+}
+
+//全排列算法
+void Swap(char *a, char *b) {
+    char t = *a;
+    *a = *b;
+    *b = t;
+}
+//在pszStr数组中，[nBegin,nEnd)中是否有数字与下标为nEnd的数字相等
+bool IsSwap(char *pszStr, int nBegin, int nEnd) {
+    for (int i = nBegin; i < nEnd; i++)
+        if (pszStr[i] == pszStr[nEnd])
+            return false;
+    return true;
+}
+//k表示当前选取到第几个数,m表示共有多少数.
+void AllRange(char *pszStr, int k, int m) {
+    if (k == m) {
+        static int s_i = 1;
+        printf("  第%3d个排列\t%s\n", s_i++, pszStr);
+    }
+    else {
+        for (int i = k; i <= m; i++) {//第i个数分别与它后面的数字交换就能得到新的排列
+            if (IsSwap(pszStr, k, i)) {
+                Swap(pszStr + k, pszStr + i);
+                AllRange(pszStr, k + 1, m);
+                Swap(pszStr + k, pszStr + i);
+            }
+        }
+    }
+}
+
+//17. Letter Combinations of a Phone Number
+vector<string> letterCombinations(string digits) {
+    string nums[]={"0","1","abc","def","ghi","jkl","mno","pqrs","uvw","xyz"};
+    vector<string> ans;
+    long length = digits.length();
+    for (int i = 0; i < length; i++) {
+        if (!ans.size()) {
+            ans.push_back("");
+        }
+        vector<string> temp;
+        for (int j = 0; j < ans.size(); j++) {
+            string str = nums[digits[i] - '0'];
+            for (int k = 0; k < str.length(); k++) {
+                temp.push_back(ans[j] + str[k]);
+            }
+        }
+        ans = temp;
+    }
+    return ans;
+}
+
 int main(int argc, const char * argv[]) {
-    int maxLength = lengthOfLongestSubstring("dvdf");
-    printf("%d\n",maxLength);
+    vector<string> ans = letterCombinations("23");
     return 0;
 }
