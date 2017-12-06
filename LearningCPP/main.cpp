94f7d22cc922919a940729863f3c177ab67671ff
lioneldy <407654339@qq.com>
2017-12-06 16:22:39 +0800

    新增LeetCode第85题

--- LearningCPP/main.cpp
+++ LearningCPP/main.cpp
@@ -1592,7 +1592,41 @@ int largestRectangleArea(vector<int> &heights) {
     return ret;
 }
 
-int main(int argc, const char * argv[]) {
+//85. Maximal Rectangle
+int maximalRectangle(vector<vector<char>>& matrix) {
+    if(matrix.empty()) return 0;
+    const int m = (int)matrix.size();
+    const int n = (int)matrix[0].size();
+    vector<int> left(n, 0);
+    vector<int> right(n, n);
+    vector<int> height(n, 0);
+    int maxA = 0;
+    for(int i = 0; i < m; i++) {
+        int cur_left = 0, cur_right = n;
+        for(int j = 0; j < n; j++) {
+            if(matrix[i][j] == '1') height[j]++;
+            else height[j] = 0;
+        }
+        for(int j = 0; j < n; j++) {
+            if(matrix[i][j] == '1') left[j] = max(left[j], cur_left);
+            else {
+                left[j] = 0;
+                cur_left = j + 1;
+            }
+        }
+        for(int j = n - 1; j >= 0; j--) {
+            if(matrix[i][j] == '1') right[j] = min(right[j], cur_right);
+            else {
+                right[j] = n;
+                cur_right = j;
+            }
+        }
+        for(int j = 0; j < n; j++)
+            maxA = max(maxA, (right[j] - left[j]) * height[j]);
+    }
+    return maxA;
+}
 
+int main(int argc, const char * argv[]) {
     return 0;
 }
